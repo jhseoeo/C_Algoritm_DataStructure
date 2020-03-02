445dccbf618c99c99d010e9a180b3307ed94d314
Jun-Hyuk Seo <junhyuk0801@naver.com>
2020-03-02 23:20:47 +0900

    20.03.02

---
+++ C_Algoritm_DataStructure/Linked_Structure/Array_Based_Linear_List/Array_Based_Linear_List.h
@@ -0,0 +1,16 @@
+/*
+	Linked list implementation based on pointer.
+*/
+
+#ifndef ARRAY_BASED_LINEAR_LIST
+#define ARRAY_BASED_LINEAR_LIST
+
+#include "../../Base/Base.h"
+
+
+typedef struct {
+
+} Node;
+
+
+#endif
